cf527c5c3efa0f5d54ae5d083d4f361222351c1e
sofjagyy <sofjagy@yandex.ru>
2025-08-20 13:34:11 +0200

    Улучшила организацию пакетов проекта

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorsHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorsHandler.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.validation;
+package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ValidationHandler {
+public class ErrorsHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse validationErrors(MethodArgumentNotValidException ex) {
