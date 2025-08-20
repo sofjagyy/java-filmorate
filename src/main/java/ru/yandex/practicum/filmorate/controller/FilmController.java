ab01d02a542dab0520cfd5f010fd8e3911419c47
sofjagyy <sofjagy@yandex.ru>
2025-08-20 13:46:07 +0200

    Убрала лишние логи

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -28,50 +28,37 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> findAll() {
-        log.info("Запрос списка всех фильмов. Количество фильмов в базе: {}", films.size());
         Collection<Film> result = films.values();
-        log.debug("Возвращаем {} фильмов", result.size());
         return result;
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         film.setId(getNextId());
-        log.debug("Присвоен ID: {}", film.getId());
-
         films.put(film.getId(), film);
-        log.info("Фильм ID: {} успешно создан и добавлен в коллекцию, всего фильмов: {}",
-                 film.getId(), films.size());
         return film;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film updatedFilm) {
-        log.info("Попытка обновления фильма с ID: {}", updatedFilm.getId());
         if (!films.containsKey(updatedFilm.getId())) {
             log.warn("Попытка обновления несуществующего фильма с ID: {}", updatedFilm.getId());
             throw new NotFoundException("Нет фильма с таким id");
         }
 
         Film oldFilm = films.get(updatedFilm.getId());
-        log.info("Фильм найден: '{}'. Начинаем обновление", oldFilm.getName());
 
         oldFilm.setName(updatedFilm.getName());
-        log.info("Обновляем название");
 
         oldFilm.setReleaseDate(updatedFilm.getReleaseDate());
-        log.info("Обновляем дату релиза");
 
         if (updatedFilm.getDescription() != null) {
             oldFilm.setDescription(updatedFilm.getDescription());
         }
-        log.info("Обновляем описание");
 
         if (updatedFilm.getDuration() != null) {
-            log.info("Обновляем длительность");
             oldFilm.setDuration(updatedFilm.getDuration());
         }
-
         log.info("Фильм с ID: {} успешно обновлен", oldFilm.getId());
         return oldFilm;
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -18,51 +18,34 @@ public class UserController {
 
     @GetMapping
     public Collection<User> findAll() {
-        log.info("Запрос списка всех пользователей. Количество пользователей в базе: {}", users.size());
         Collection<User> result = users.values();
-        log.debug("Возвращаем {} пользователей", result.size());
         return result;
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        log.info("Создание нового пользователя в базе: '{}'", user.getLogin());
-
         user.setId(getNextId());
-        log.debug("Присвоен ID: {} пользователю '{}'", user.getId(), user.getLogin());
 
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
-
         users.put(user.getId(), user);
-        log.info("Пользователь '{}' ID: {} успешно создан и добавлен в базу, всего пользователей: {}",
-                user.getLogin(), user.getId(), users.size());
         return user;
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User updatedUser) {
-        log.info("Попытка обновления данных пользователя с ID: {}", updatedUser.getId());
         if (!users.containsKey(updatedUser.getId())) {
             log.warn("Попытка обновления несуществующего пользователя с ID: {}", updatedUser.getId());
             throw new NotFoundException("Нет пользователя с таким id");
         }
 
         User oldUser = users.get(updatedUser.getId());
-        log.info("Пользователь найден: '{}'. Начинаем обновление", oldUser.getLogin());
-
         oldUser.setName(updatedUser.getName());
-        log.info("Обновляем имя");
-
         oldUser.setEmail(updatedUser.getEmail());
-        log.info("Обновляем электронную почту");
-
         oldUser.setLogin(updatedUser.getLogin());
-        log.info("Обновляем логин");
 
         if (updatedUser.getBirthday() != null) {
-            log.info("Обновляем день рождения пользователя");
             oldUser.setBirthday(updatedUser.getBirthday());
         }
 
