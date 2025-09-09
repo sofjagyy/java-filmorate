ecc65dba02dfae9c0b651c6aca367e92b8455fec
sofjagyy <sofjagy@yandex.ru>
2025-09-09 20:11:21 +0200

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,10 +5,10 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film add(Film film);
-    
+
     Film update(Film film);
-    
+
     Collection<Film> findAll();
-    
+
     Film findById(Long id);
 }
